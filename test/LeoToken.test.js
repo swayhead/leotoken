2448e711b407a81a83949daddd20b349770985f7
Leon <l.troitski@gmail.com>
2021-06-03 09:09:15 +0200

    beforeEach

--- test/LeoToken.test.js
+++ test/LeoToken.test.js
@@ -12,24 +12,28 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
+beforeEach(async ()=>{
+    this.myToken = await Token.new(1000000);
+})
+
 contract("TokenTest", async accounts=>{
     const [deployerAccount, recepient, anotherAccount] = accounts;
 
     it("All tokens should be in my account", async ()=>{
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
     it("It is possible to send tokens between accounts", async ()=>{
         const tokensToSend = 1;
-        let instance = await Token.deployed();
+        let instance =this.myToken;
         await instance.transfer(recepient, tokensToSend);
         return expect(instance.balanceOf(recepient)).to.eventually.be.a.bignumber.equal(new BN(tokensToSend));
     })
 
     it("It is not possible to send more tokens than available in total", async ()=>{
-        let instance = await Token.deployed();
+        let instance = this.myToken;
         let balanceOfDeployer = await instance.balanceOf(deployerAccount);
         return expect(instance.transfer(recepient, new BN(balanceOfDeployer + 1))).to.eventually.be.rejected;
     })
